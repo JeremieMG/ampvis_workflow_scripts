436def7fb893a78d306a787f89b11e6281c63701
JeremieMG <malengreau.jeremie@gmail.com>
2017-04-21 16:24:03 +0200

    Update REWORK_16S.V13.workflow_v4.1.sh

--- REWORK_16S.V13.workflow_v4.1.sh
+++ REWORK_16S.V13.workflow_v4.1.sh
@@ -7,5 +7,13 @@ echo ""
 echo "Finding your samples and copying them to the current folder"
 while read samples
 do
-a="_";
-NAME=$samples$a;
+a="_" ;
+NAME=$samples$a ;
+head -n 1 $NAME*R1* | sed 's/\@/>/' >> id.txt ;
+head -n 200000 $NAME*R1* >> forward.fastq
+head -n 200000 $NAME*R2* >> reverse.fastq
+gzip $NAME*R1* ; 
+done < samples
+
+paste -d "\t" id.txt samples > sampleid.txt
+date
